f6f3d4de994aca3674cfd00529f1b24735b54348
sh4dex <sthx09567541@gmail.com>
2026-04-19 19:56:40 -0500

    NFTCollection done

--- src/NFTCollection.sol
+++ src/NFTCollection.sol
@@ -1,14 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.33;
 
-import {
-    Ownable
-} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
-import {
-    IERC721
-} from "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import {ReentrancyGuard} from "../lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract NFTCollection is Ownable {
+contract NFTCollection is Ownable, ReentrancyGuard {
+    //TODO: Implement fees percentage
     struct Listing {
         address seller;
         address nftAddress;
@@ -16,7 +14,22 @@ contract NFTCollection is Ownable {
         uint256 price;
     }
 
-    mapping(address => mapping(uint256 => Listing)) listing;
+    mapping(address => mapping(uint256 => Listing)) listings;
+
+    event NftListed(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event CanceledListing(address indexed nftAddress, uint256 indexed tokenId);
+    event NftSold(
+        address indexed seller,
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 tokenId,
+        uint256 price
+    );
 
     constructor() Ownable(msg.sender) {}
 
@@ -26,7 +39,7 @@ contract NFTCollection is Ownable {
         address nftAddress_,
         uint256 tokenId_,
         uint256 price_
-    ) public {
+    ) external nonReentrant {
         require(price_ > 0, "Price can't be 0");
         require(
             IERC721(nftAddress_).ownerOf(tokenId_) == msg.sender,
@@ -40,8 +53,47 @@ contract NFTCollection is Ownable {
             price: price_
         });
 
-        listing[nftAddress_][tokenId_] = listing_;
+        listings[nftAddress_][tokenId_] = listing_;
+        emit NftListed(msg.sender, nftAddress_, tokenId_, price_);
     }
-    // buy
-    // cancel listing
+
+    function buyNft(
+        address nftAddress_,
+        uint256 tokenId_
+    ) external payable nonReentrant {
+        Listing memory listing_ = listings[nftAddress_][tokenId_];
+        require(
+            listing_.seller != address(0) && listing_.price > 0,
+            "NFT not listed for sale"
+        );
+        require(listing_.price == msg.value, "Incorrect payment amount");
+
+        delete listings[nftAddress_][tokenId_];
+
+        IERC721(nftAddress_).safeTransferFrom(
+            listings[nftAddress_][tokenId_].seller,
+            msg.sender,
+            listing_.tokenId
+        );
+
+        (bool success, ) = listing_.seller.call{value: listing_.price}("");
+        require(success, "transfer failed");
+        emit NftSold(
+            msg.sender,
+            listing_.seller,
+            listing_.nftAddress,
+            listing_.tokenId,
+            listing_.price
+        );
+    }
+
+    function cancelListing(
+        address nftAddress_,
+        uint256 tokenId_
+    ) external nonReentrant {
+        require(listings[nftAddress_][tokenId_].seller == msg.sender);
+        delete listings[nftAddress_][tokenId_];
+
+        emit CanceledListing(nftAddress_, tokenId_);
+    }
 }
